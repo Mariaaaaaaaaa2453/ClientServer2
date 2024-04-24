5f81759e11514308b70dfe89b72d95766d7ef283
“Maria <mashabaryakhtar@Mashas-MacBook-Pro.local>
2024-04-24 09:41:15 +0200

    Client

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,110 +1,87 @@
-#define WIN32_LEAN_AND_MEAN
-
 #include <iostream>
-#include <windows.h>
-#include <ws2tcpip.h>
 #include <string>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
 using namespace std;
 
-#pragma comment (lib, "Ws2_32.lib")
-#pragma comment (lib, "Mswsock.lib")
-#pragma comment (lib, "AdvApi32.lib")
+#pragma comment(lib, "Ws2_32.lib")
 
-#define DEFAULT_BUFLEN 512
-#define DEFAULT_PORT "27015"
+constexpr auto MAX_BUFFER = 512;
+constexpr char SERVER_PORT[] = "27015";
+constexpr char SERVER_ADDRESS[] = "127.0.0.1";
 
-int main(int argc, char** argv)
-{
-    setlocale(0, "");
-    system("title CLIENT SIDE");
-
+void initializeWinsock() {
     WSADATA wsaData;
-    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if (iResult != 0) {
-        cout << "WSAStartup failed with error: " << iResult << "\n";
-        return 11;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        throw runtime_error("Winsock initialization failed.");
     }
+}
 
-    addrinfo hints;
-    ZeroMemory(&hints, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_protocol = IPPROTO_TCP;
-
-    const char* ip = "localhost";
-
-    addrinfo* result = NULL;
-    iResult = getaddrinfo(ip, DEFAULT_PORT, &hints, &result);
-
-    if (iResult != 0) {
-        cout << "getaddrinfo failed with error: " << iResult << "\n";
+SOCKET createAndConnectSocket(const addrinfo* addr) {
+    SOCKET sock = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+    if (sock == INVALID_SOCKET) {
         WSACleanup();
-        return 12;
+        throw runtime_error("Failed to create socket.");
     }
 
-    SOCKET ConnectSocket = INVALID_SOCKET;
-
-    for (addrinfo* ptr = result; ptr != NULL; ptr = ptr->ai_next) {
-        ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
-
-        if (ConnectSocket == INVALID_SOCKET) {
-            cout << "socket failed with error: " << WSAGetLastError() << "\n";
-            WSACleanup();
-            return 13;
-        }
-
-        iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
-        if (iResult == SOCKET_ERROR) {
-            closesocket(ConnectSocket);
-            ConnectSocket = INVALID_SOCKET;
-            continue;
-        }
-        break;
-    }
-
-    freeaddrinfo(result);
-
-    if (ConnectSocket == INVALID_SOCKET) {
-        cout << "невозможно подключиться к серверу. убедитесь, что процесс сервера запущен!\n";
+    if (connect(sock, addr->ai_addr, static_cast<int>(addr->ai_addrlen)) == SOCKET_ERROR) {
+        closesocket(sock);
         WSACleanup();
-        return 14;
+        throw runtime_error("Failed to connect to server.");
     }
+    return sock;
+}
 
-    //////////////////////////////////////////////////////////
-
-    while (true)
-    {
-        string message;
-        cout << "Пожалуйста, введите число: ";
-        getline(cin, message);
-
-        iResult = send(ConnectSocket, message.c_str(), (int)message.size(), 0);
-        if (iResult == SOCKET_ERROR) {
-            cout << "отправить не удалось с ошибкой: " << WSAGetLastError() << "\n";
-            closesocket(ConnectSocket);
-            WSACleanup();
-            return 15;
-        }
-
-        char response[DEFAULT_BUFLEN];
-
-        iResult = recv(ConnectSocket, response, DEFAULT_BUFLEN, 0);
-        response[iResult] = '\0';
-
-        if (iResult > 0) {
-            double serverResponse = atof(response);
-            cout << "Сервер ответил: " << serverResponse << "\n";
-        }
-        else if (iResult == 0) {
-            cout << "Соединение с сервером закрыто.\n";
-            break; // Завершення циклу, оскільки з'єднання закрито
-        }
-        else {
-            cout << "получение не удалось с ошибкой: " << WSAGetLastError() << "\n";
-            break; // Завершення циклу при помилці отримання даних
-        }
-    }
-
-    closesocket(ConnectSocket);
+void cleanUp(SOCKET sock) {
+    closesocket(sock);
     WSACleanup();
+}
+
+int main() {
+    try {
+        initializeWinsock();
+        struct addrinfo hints{}, *serverInfo;
+
+        ZeroMemory(&hints, sizeof(hints));
+        hints.ai_family = AF_UNSPEC;
+        hints.ai_socktype = SOCK_STREAM;
+        hints.ai_protocol = IPPROTO_TCP;
+
+        if (getaddrinfo(SERVER_ADDRESS, SERVER_PORT, &hints, &serverInfo) != 0) {
+            WSACleanup();
+            throw runtime_error("Failed to get server address info.");
+        }
+
+        SOCKET clientSocket = createAndConnectSocket(serverInfo);
+        freeaddrinfo(serverInfo);
+
+        string userInput;
+        char serverResponse[MAX_BUFFER];
+        while (true) {
+            cout << "Enter message: ";
+            getline(cin, userInput);
+
+            if (send(clientSocket, userInput.c_str(), static_cast<int>(userInput.length()), 0) == SOCKET_ERROR) {
+                throw runtime_error("Failed to send message.");
+            }
+
+            int bytesReceived = recv(clientSocket, serverResponse, MAX_BUFFER, 0);
+            if (bytesReceived > 0) {
+                serverResponse[bytesReceived] = '\0';
+                cout << "Received from server: " << serverResponse << endl;
+            } else if (bytesReceived == 0) {
+                cout << "Server closed the connection." << endl;
+                break;
+            } else {
+                throw runtime_error("Failed to receive message.");
+            }
+        }
+
+        cleanUp(clientSocket);
+    } catch (const exception& e) {
+        cerr << "Error: " << e.what() << endl;
+        return 1;
+    }
+    return 0;
 }
